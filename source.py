cfc31594117857b7c0b40b26175edd4c89a915d2
SwiftyKey <slipperyfish9@gmail.com>
2020-10-24 17:00:39 +0500

    change

--- source.py
+++ source.py
@@ -1,1 +1,45 @@
-print("Hello")
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QMainWindow
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+import random
+
+
+class GraphWidget(QWidget):
+    def __init__(self, parent=None):
+        super(GraphWidget, self).__init__(parent)
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.button = QPushButton('Plot')
+        self.button.clicked.connect(self.plot)
+        layout = QVBoxLayout()
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+    def plot(self):
+        data = [random.random() for _ in range(10)]
+        self.figure.clear()
+        ax = self.figure.add_subplot(111)
+        ax.plot(data, '*-')
+        self.canvas.draw()
+
+
+class Window(QWidget):
+    def init(self):
+        super().init()
+        uic.loadUi("ui.ui", self)
+        self.graph = GraphWidget()
+        self.graph.setGeometry(300, 10, 700, 600)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = Window()
+    main.show()
+    sys.exit(app.exec_())
