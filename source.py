ccb4ae513b10494c60f4789472e704a5a6cabee1
SwiftyKey <slipperyfish9@gmail.com>
2020-10-25 20:09:57 +0500

    добавлена функция add

--- source.py
+++ source.py
@@ -1,6 +1,6 @@
-import sqlite3
 import sys
 
+from widgets import *
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
@@ -9,47 +9,57 @@ class Window(QMainWindow):
     def __init__(self):
         super(QMainWindow, self).__init__()
 
+        self.user_id = 1
+
         self.con = sqlite3.connect("Cost.db")
-        cur = self.con.cursor()
 
         self.title = ["Категория", "Дата", "Цена"]
-        self.table = cur.execute("SELECT CategoryId, Date, SumCost FROM Cost").fetchall()
+        self.table = self.getCostData()
 
-        uic.loadUi("window.ui", self)
+        uic.loadUi("main_window.ui", self)
 
         self.setWindowTitle("Учет расходов")
-
-        self.tableWidget.setRowCount(len(self.table))
-        self.tableWidget.setColumnCount(len(self.title))
-        self.tableWidget.setHorizontalHeaderLabels(self.title)
 
         # self.graph = GraphWidget(self.centralWidget())
         # self.graph.setGeometry(440, 10, 751, 771)
 
-        self.show_records(self.table)
+        self.showNotes()
 
         self.button_add.clicked.connect(self.add)
         self.button_remove.clicked.connect(self.remove)
         self.button_edit.clicked.connect(self.edit)
 
-    def show_records(self, table):
-        for i, row in enumerate(table):
+    def getCostData(self):
+        cur = self.con.cursor()
+        result = cur.execute(f'''SELECT Title, Date, SumCost FROM Cost INNER JOIN Category ON 
+Cost.CategoryId = Category.CategoryId WHERE UserId = {self.user_id}''').fetchall()
+        cur.close()
+
+        return result
+
+    def showNotes(self):
+        self.tableWidget.setRowCount(len(self.table))
+        self.tableWidget.setColumnCount(len(self.title))
+        self.tableWidget.setHorizontalHeaderLabels(self.title)
+
+        for i, row in enumerate(self.table):
             for j, value in enumerate(row):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(value)))
 
     def sort(self):
         pass
 
-    def add(self, event):
-        pass
+    def add(self):
+        new_note_form = NoteWindow(self.user_id, self)
+        new_note_form.exec_()
+
+        self.table = self.getCostData()
+        self.showNotes()
 
     def remove(self):
         pass
 
     def edit(self):
-        pass
-
-    def add_category(self):
         pass
 
 
--- widgets.py
+++ widgets.py
@@ -1,8 +1,60 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5 import uic
+from PyQt5.QtCore import QDate
+from PyQt5.QtWidgets import QWidget, QDialog, QInputDialog
+from datetime import date
+
+import sqlite3
 
 
-class RecordWidget(QWidget):
-    pass
+class NoteWindow(QDialog):
+    def __init__(self, user_id, parent=None):
+        super(NoteWindow, self).__init__(parent)
+
+        self.con = sqlite3.connect("Cost.db")
+
+        self.user_id = user_id
+        self.category = ""
+        self.date = ""
+        self.cost = ""
+
+        uic.loadUi("new_note_window.ui", self)
+        self.setWindowTitle("Новая запись")
+
+        cur = self.con.cursor()
+        categories = map(lambda item: item[0], cur.execute("SELECT Title FROM Category").fetchall())
+        self.select_category.addItems(categories)
+
+        self.select_date.setMinimumDate(QDate(date.today()))
+
+        self.button_add.clicked.connect(self.add_note)
+        self.button_create_category.clicked.connect(self.new_category)
+
+    def new_category(self):
+        title = QInputDialog.getText(self, "Новая категория",
+                                     "Введите название новой категории")
+        if title[0] and title[1]:
+            cur = self.con.cursor()
+            cur.execute(f"INSERT INTO Category(Title) VALUES('{title[0]}')")
+            cur.close()
+            self.con.commit()
+
+            self.select_category.insertItem(0, title[0])
+            self.select_category.setCurrentIndex(0)
+
+    def add_note(self):
+        cur = self.con.cursor()
+
+        self.category = cur.execute(f'''SELECT CategoryId FROM Category 
+WHERE Title = "{self.select_category.currentText()}"''').fetchone()[0]
+        self.date = self.select_date.date().toString('yyyy-MM-dd')
+        self.cost = self.select_cost.value()
+
+        cur.execute(f'''INSERT INTO Cost(UserId, CategoryId, Date, SumCost) 
+VALUES({self.user_id}, {self.category}, "{self.date}", {self.cost})''')
+        cur.close()
+        self.con.commit()
+
+        self.close()
 
 
 class LoginWidget(QWidget):
