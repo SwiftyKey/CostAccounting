7a77dfd30dc5b6ca385c76eef442ffcae27fe78d
SwiftyKey <slipperyfish9@gmail.com>
2020-10-25 17:34:51 +0500

    change

--- source.py
+++ source.py
@@ -3,48 +3,6 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
-import random
-
-
-class GraphWidget(QWidget):
-    def __init__(self, parent=None):
-        super(GraphWidget, self).__init__(parent)
-
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.button = QPushButton('Plot')
-        self.button.clicked.connect(self.plot)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
-
-        self.setLayout(layout)
-
-    def plot(self):
-        data = [random.random() for i in range(10)]
-        self.figure.clear()
-        ax = self.figure.add_subplot(111)
-        ax.plot(data, '*-')
-        self.canvas.draw()
-
-
-class AddRecordWidget(QWidget):
-    pass
-
-
-class LoginWidget(QWidget):
-    pass
-
-
-class Registration:
-    pass
 
 
 class Window(QMainWindow):
@@ -54,10 +12,10 @@ class Window(QMainWindow):
         self.con = sqlite3.connect("Cost.db")
         cur = self.con.cursor()
 
-        self.title = ["ID", "Категория", "Дата", "Цена"]
-        self.table = cur.execute("SELECT UserId, CategoryId, Date, SumCost FROM Cost").fetchall()
+        self.title = ["Категория", "Дата", "Цена"]
+        self.table = cur.execute("SELECT CategoryId, Date, SumCost FROM Cost").fetchall()
 
-        uic.loadUi("ui.ui", self)
+        uic.loadUi("window.ui", self)
 
         self.setWindowTitle("Учет расходов")
 
@@ -65,8 +23,8 @@ class Window(QMainWindow):
         self.tableWidget.setColumnCount(len(self.title))
         self.tableWidget.setHorizontalHeaderLabels(self.title)
 
-        self.graph = GraphWidget(self.centralWidget())
-        self.graph.setGeometry(440, 10, 751, 771)
+        # self.graph = GraphWidget(self.centralWidget())
+        # self.graph.setGeometry(440, 10, 751, 771)
 
         self.show_records(self.table)
 
---
+++ widgets.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QWidget
+
+
+class RecordWidget(QWidget):
+    pass
+
+
+class LoginWidget(QWidget):
+    pass
+
+
+class Registration:
+    pass
