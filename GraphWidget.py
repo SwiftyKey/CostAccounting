6ae15cef19cac69ba9f92757cf3ae945753f3dd8
fearppen <antonchachylin@gmail.com>
2020-10-30 12:25:14 +0500

    add cooments

--- GraphWidget.py
+++ GraphWidget.py
@@ -9,16 +9,16 @@ from datetime import date
 import numpy as np
 
 
-def sort_list_dates(dates):
+def sort_list_dates(dates):  # функция для сортировки списка дат
     return sorted(dates, key=lambda x: str_date_to_datetime(x))
 
 
-def str_date_to_datetime(date_1):
+def str_date_to_datetime(date_1):  # функция приведения даты к формату date
     day, month, year = map(int, date_1.split('.'))
     return date(year, month, day)
 
 
-def list_dates_to_format(dates):
+def list_dates_to_format(dates):  # функция приведения дат "yyyy-mm-dd" к формату "dd.mm.yyyy"
     for i in range(len(dates)):
         for j in range(len(dates[i])):
             year, month, day = list(map(int, dates[i][j].split('-')))
@@ -26,40 +26,41 @@ def list_dates_to_format(dates):
     return dates
 
 
-def format_string(pct, number):
+def format_string(pct, number):  # функция форматирования строки для создания круговой диаграммы
     absolute = int(pct / 100. * np.sum(number))
     return "{:d} руб.\n({:.1f}%)".format(absolute, pct)
 
 
-def do_data_to_format_bar_and_plot_graph(data, labels, dates):
+def do_data_to_format_bar_and_plot_graph(data, labels, dates):  # функция приведения полученных данных к удобному виду
+    # для построения столбчатой диаграммы и графика
     format_list = []
     all_dates = []
-    for i in dates:
+    for i in dates:  # создается список всех дат
         for j in i:
             if j not in all_dates:
                 all_dates.append(j)
     all_dates = sort_list_dates(all_dates)
-    if labels:
+    if labels:  # брезается категория "иное", нужная только для построения круговой диаграммы
         if labels[-1] == 'Иное':
             labels = labels[:-1]
             data = data[:-1]
 
     for i in range(len(labels)):
         union_data_and_dates = zip(data[i], dates[i])
-        sorted_union_data_and_dates = sorted(union_data_and_dates,
-                                             key=lambda tup: (str_date_to_datetime(tup[1]), tup[0]))
+        sorted_union_data_and_dates = sorted(union_data_and_dates,  # сортировка для того, чтобы покупки соответствовали
+                                             key=lambda tup: (str_date_to_datetime(tup[1]), tup[0]))  # датам
         sorted_dates = all_dates
         sorted_data = [0] * len(all_dates)
-        for j in sorted_union_data_and_dates:
-            sorted_data[sorted_dates.index(j[1])] += j[0]
+        for j in sorted_union_data_and_dates:  # цикл для того, чтобы не было случая, когда покупок больше чем дат
+            sorted_data[sorted_dates.index(j[1])] += j[0]  # иначе построение графика окажется невозможным
 
         format_list.append((sorted_data, sorted_dates, labels[i]))
 
-    return format_list, all_dates
+    return format_list, all_dates  # возвращает еще все даты, чтобы в дальнейшнем не пришлось их считать заново
 
 
-def do_data_to_format_pie_graph(data):
-    format_data = []
+def do_data_to_format_pie_graph(data):  # функция приведения данных к формату, удобному для
+    format_data = []  # построения круговой диаграммы
     for i in range(len(data)):
         format_data.append(sum(data[i]))
     return format_data
@@ -112,34 +113,34 @@ class GraphWidget(QWidget):
                                     textprops=dict(color="w"))
             ax.legend(patches, labels_graph, title="Расходы", loc="lower right",
                       bbox_to_anchor=(1, 0, 0.5, 1))
-            ax.set_title("Круговая диаграмма, построенная по вашим расходам:")
+            ax.set_title("Круговая диаграмма ваших расходов:")
             self.canvas.draw()
-        else:
+        else:  # иначе выводим надпись о том, что не найдены данные, удовлетворяющие запросу
             self.figure.clear()
             self.label_if_not_found_inf.setText("Не было найдено информации, "
                                                 "убедитесь, что данные введены верно и повторите запрос.")
-            #  иначе выводим надпись о том, что не найдены данные, удовлетворяющие запросу
 
-    def build_bar_plot(self):
+    def build_bar_plot(self):  # функция для построения столбчатой диаграммы
         self.figure.clear()
         data, labels_graph, dates = self.find_information_for_graph()
-        data_to_build_graph, all_dates = do_data_to_format_bar_and_plot_graph(data, labels_graph,
-                                                                              list_dates_to_format(
-                                                                                  dates))
+        data_to_build_graph, all_dates = do_data_to_format_bar_and_plot_graph(data,
+                                                                              labels_graph,
+                                                                              list_dates_to_format(dates))
         if labels_graph:
             self.label_if_not_found_inf.setText("")
             ax = self.figure.add_subplot(111)
 
-            values = [0] * len(all_dates)
-            all_dates = sort_list_dates(all_dates)
+            values = [0] * len(all_dates)  # значения для аргумента bottom, чтобы столбцы не наслаивались друг на друга
+
             for i in range(len(data_to_build_graph)):
                 ax.bar(data_to_build_graph[i][1], data_to_build_graph[i][0],
                        width=0.25, bottom=values, label=data_to_build_graph[i][2])
-                for val in range(len(data_to_build_graph[i][1])):
-                    values[all_dates.index(data_to_build_graph[i][1][val])] += \
-                    data_to_build_graph[i][0][val]
 
-            ax.set_title("Гистограмма, построенная по вышим расходам:")
+                for val in range(len(data_to_build_graph[i][1])):  # обновляются значения values для аргумента bottom
+                    values[all_dates.index(data_to_build_graph[i][1][val])] += \
+                        data_to_build_graph[i][0][val]
+
+            ax.set_title("Гистограмма ваших расходов:")
             ax.set_ylabel("Затраты")
             ax.set_xlabel("Даты покупок")
             ax.legend()
@@ -157,10 +158,10 @@ class GraphWidget(QWidget):
         if labels_graph:
             self.label_if_not_found_inf.setText("")
             ax = self.figure.add_subplot(111)
-            for i in range(len(data_to_build_graph)):
+            for i in range(len(data_to_build_graph)):  # строятся графики расходов по категориям по очереди
                 ax.plot(data_to_build_graph[i][1], data_to_build_graph[i][0], "o-",
                         label=data_to_build_graph[i][2])
-            ax.set_title("График, построенный по вашим расходам:")
+            ax.set_title("График ваших расходов:")
             ax.set_ylabel("Затраты")
             ax.set_xlabel("Даты покупок")
             ax.legend()
@@ -191,21 +192,19 @@ class GraphWidget(QWidget):
             category = cur.execute("""SELECT Title FROM Category WHERE CategoryId = ?""",
                                    (result[i][2],)).fetchone()[0]
             if category not in labels:
-                if category in self.list_categories:
-                    labels.append(category)
+                if category in self.list_categories:  # заполняются двумерные списки дат и значений, соответствующих
+                    labels.append(category)  # друг другу
                     data.append([result[i][4]])
                     dates.append([result[i][3]])
                 else:
-                    something += result[i][4]
+                    something += result[i][4]  # заполняется значение иных категорий, не выбранных пользователем
             else:
                 data[labels.index(category)].append(result[i][4])
                 dates[labels.index(category)].append(result[i][3])
         con.close()
 
         if something != 0 and data:
-            y = []
-            y.append(something)
-            data.append(y)
+            data.append([something])
             labels.append("Иное")
 
         return data, labels, dates
@@ -219,7 +218,7 @@ class GraphWidget(QWidget):
                 list_categories.append(list_item.text())
         first_date = self.dateEdit.date()  # возвращает объект типа QDate
         last_date = self.dateEdit_2.date()  # возвращает объект типа QDate
-        diagram = self.comboBox.currentIndex()
+        diagram = self.comboBox.currentIndex()  # индекс графика
         return first_date.year(), first_date.month(), first_date.day(), \
                last_date.year(), last_date.month(), last_date.day(), \
                diagram, list_categories  # раскладываем даты на три составляющих для дальнейших операций с ними
